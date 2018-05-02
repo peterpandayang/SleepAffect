ca189f918d4901c7e9ab15c5aa784b31fd4c242f
peterpandayang <byang14@dons.usfca.edu>
2018-05-02 14:12:01 -0700

    working version for NoPuppy

--- app/src/main/java/com/example/bingkunyang/teenplus/com/example/bingkunyang/teenplus/fragment/MoodFragment.java
+++ app/src/main/java/com/example/bingkunyang/teenplus/com/example/bingkunyang/teenplus/fragment/MoodFragment.java
@@ -1,5 +1,6 @@
 package com.example.bingkunyang.teenplus.com.example.bingkunyang.teenplus.fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.bingkunyang.teenplus.R;
 import com.example.bingkunyang.teenplus.com.example.bingkunyang.teenplus.model.Record;
@@ -22,8 +24,6 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by bingkunyang on 4/9/18.
@@ -36,6 +36,8 @@ public class MoodFragment extends Fragment {
     private ImageView imageView;
     List<Record> records = new LinkedList<>();
     private Button showMoodBtn;
+    MediaPlayer mediaPlayer;
+    private TextView textView;
 
     public static MoodFragment newInstance() {
         return new MoodFragment();
@@ -74,13 +76,16 @@ public class MoodFragment extends Fragment {
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_mood, container, false);
         showMoodBtn = (Button)rootView.findViewById(R.id.show_mood);
-        imageView = (ImageView) rootView.findViewById(R.id.puppy_image);
+//        imageView = (ImageView) rootView.findViewById(R.id.puppy_image);
+
+        textView = rootView.findViewById(R.id.text_view_id);
 
         // set on click listener
         showMoodBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                showMode();
+//                showMode();
+                showText();
             }
         });
 
@@ -89,26 +94,89 @@ public class MoodFragment extends Fragment {
 
 
 
-    private void showMode() {
+//    private void showMode() {
+//        // retrieve the user's previous sleep
+//        if(records.size() == 0){
+//            System.out.println("there are no records");
+//            imageView.setImageResource(R.drawable.puppy_5);
+//        }
+//        else if(records.size() == 1){
+//            System.out.println("there is only one record for the user");
+//            imageView.setImageResource(R.drawable.puppy_5);
+//            mediaPlayer.start();
+//        }
+//        else{
+//            // poll out two record and compare
+//            System.out.println("there are multiple record for the user");
+//            int size = records.size();
+//            Record prevprev = records.get(size - 2);
+//            Record prev = records.get(size - 1);
+//            System.out.println("prevprev date is: " + prevprev.getTime());
+//            System.out.println("prev date is: " + prev.getTime());
+//            helper(imageView, prevprev.getTime(), prev.getTime());
+//        }
+//    }
+
+    private void showText() {
         // retrieve the user's previous sleep
         if(records.size() == 0){
             System.out.println("there are no records");
-        }
-        else if(records.size() == 1){
-            System.out.println("there is only one record for the user");
-            imageView.setImageResource(R.drawable.puppy_5);
+//            imageView.setImageResource(R.drawable.puppy_5);
         }
         else{
-            // poll out two record and compare
-            System.out.println("there are multiple record for the user");
             int size = records.size();
-            Record prevprev = records.get(size - 2);
             Record prev = records.get(size - 1);
-            System.out.println("prevprev date is: " + prevprev.getTime());
-            System.out.println("prev date is: " + prev.getTime());
+            textView.setText(prev.time);
+        }
+//        else if(records.size() == 1){
+//            System.out.println("there is only one record for the user");
+//            imageView.setImageResource(R.drawable.puppy_5);
+//        }
+//        else{
+//            // poll out two record and compare
+//            System.out.println("there are multiple record for the user");
+//            int size = records.size();
+//            Record prevprev = records.get(size - 2);
+//            Record prev = records.get(size - 1);
+//            System.out.println("prevprev date is: " + prevprev.getTime());
+//            System.out.println("prev date is: " + prev.getTime());
+////            helper(imageView, prevprev.getTime(), prev.getTime());
+//        }
+    }
+
+    private void helper(ImageView imageView, String prevprev, String prev){
+        boolean ppGood = getBoolean(prevprev);
+        boolean pGood = getBoolean(prev);
+        if(ppGood && pGood){
+            imageView.setImageResource(R.drawable.puppy_8);
+            mediaPlayer.start();
+        }
+        else if(!ppGood && pGood){
+            imageView.setImageResource(R.drawable.puppy_7);
+            mediaPlayer.start();
+        }
+        else if(ppGood && !pGood){
+            imageView.setImageResource(R.drawable.puppy_3);
+        }
+        else{
+            int prevprevHour = Integer.parseInt(prevprev.split(" ")[3].split(":")[0]);
+            int prevHour = Integer.parseInt(prev.split(" ")[3].split(":")[0]);
+            if(prevHour <= prevprevHour){
+                imageView.setImageResource(R.drawable.puppy_5);
+            }
+            else {
+                imageView.setImageResource(R.drawable.puppy_1);
+            }
         }
     }
 
+    private boolean getBoolean(String date){
+        String time = date.split(" ")[3];
+        System.out.println("time is : " + time);
+        String hour = time.split(":")[0];
+        System.out.println("hour is: " + hour);
+        return Integer.parseInt(hour) > 9;
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
